38876a88d379b67edbcaceda3c78d0882d762abf
fabraosaad <fernando.abraosaad@kireygroup.com>
2020-11-02 12:40:23 +0100

    +Proj

--- Auto/src/automationFramework/FirstTestCase.java
+++ Auto/src/automationFramework/FirstTestCase.java
@@ -51,14 +51,34 @@ public class FirstTestCase {
 		   Select piuRecenti1 = new Select(driver.findElement(By.xpath("/html/body/div[1]/div/main/div/div[1]/div[1]/select")));
 		   piuRecenti1.selectByIndex(2);
 		   			Thread.sleep(5000);
-		   			
-		   		//*[@id="app"]/div/main/div/div[2]/div/div[2]/a/div[2]/ul/li[1]
-		   			///html/body/div[1]/div/main/div/div[2]/div/div[2]/a/div[2]/ul/li[1]
-		   List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + "2015" + "')]"));
-		  // list.
-		   //Assert.assertFalse(true);("Text not found!", list.size() > 0);
-		  // Assert.assertEquals(actual, expected);
-		// driver.quit();
+		  
+		  //Verify Year			
+		  if(driver.getPageSource().contains("2015")&driver.getPageSource().contains("2016")&driver.getPageSource().contains("2017")&driver.getPageSource().contains("2018")&driver.getPageSource().contains("2019")&driver.getPageSource().contains("2020")) {
+		   		System.out.println("Verifica Anno Passed");
+		   		}else{
+		   		System.out.println("Verifica Anno Failed");		
+		   		}
+		  //Verify Price Range
+		// scrape price elements
+/*		  List<WebElement> price = driver.findElements(By.xpath("//span[@class='find_prices']"));
+
+		  // extract the prices from the price elements and store in a List
+		  List<String> prices = new ArrayList<String>();
+		  for (WebElement e : price)
+		  {
+		      prices.add(e.getText());
+		  }
+
+		  // make a copy of the list
+		  List<String> sortedPrices = new ArrayList<String>(prices);
+
+		  // sort the list
+		  Collections.sort(sortedPrices);
+
+		  // true if the prices are sorted
+		  System.out.println(sortedPrices.equals(prices));*/
+		  
+		 driver.quit();
 		 System.out.println("Execution complete");
 	}
 
